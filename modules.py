8a1eb51ad7c5e711047a38ce58753fd3baf3c4a6
Kenshin Himura <thephpman@users.noreply.github.com>
2020-09-18 04:58:08 +0600

    added flask template
    Initial testing with flask, now i'ts failing

--- modules.py
+++ modules.py
@@ -1,4 +1,13 @@
-def main():
+import requests
+import json
+
+# kodi communication using jesonrpc interface
+# retriving the now plying data from kodi.
+# the next steps is to send those data to my google calender
+
+#Need to learn -
+# google calender api
+def what_in_kodi():
     url = "http://192.168.0.2:8080/jsonrpc"
 
     # Example echo method
@@ -7,7 +16,6 @@ def main():
     # print(json.dumps(response['result'], indent=4))
     with open('response.json', 'w') as outfile:
         json.dump(response['result'], outfile)
-def now_playing(currently_watching playing_season, playing_epoisode):
     with open('response.json') as playing_now:
         read_content = json.load(playing_now)
         showname_detail = read_content['item']
@@ -17,4 +25,8 @@ def now_playing(currently_watching playing_season, playing_epoisode):
         currently_watching = showname
         playing_season = season
         playing_epoisode = episode
-        return print("Currently Playing {} Season {} Episode {}".format(currently_watching, playing_season, playing_epoisode))
+        return("Currently Playing {} Season {} Episode {}".format(currently_watching, playing_season, playing_epoisode))
+if __name__ == "__main__":
+    what_in_kodi()
+now_playing = what_in_kodi()
+print(now_playing)
