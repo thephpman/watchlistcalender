68dd69f0b5b54da64a4b4118564990eafba721a3
Kenshin Himura <thephpman@users.noreply.github.com>
2020-09-19 04:34:47 +0600

    api addend
    created api file to retrive and send data

--- README.md
+++ README.md
@@ -4,9 +4,19 @@ Initially some random testing code.
 After a full functioning raw code my plan is to convert this hole project to a full responsive calender program. 
 currently thinking about flask and django. Although I have to learn it. 
 ##### Questions I have now:
-* How to i send this data to my google calender ? (maybe Google calender api!)
+-[x] How to i send this data to my google calender ? (maybe Google calender api!) 
+     ##### New problem arise:
+     -[ ] Although i am able to push my watched episode to calender but i am only 
+able to send the system current time, I need to learn how can i pass the actual playing time 
+and playing duration time. So that if my script sun at the middle of the episode it will grab the start time and actual ending time and send those
+to google calender. 
 * Should i store those to a database or i just send them on the fly ? 
 * From when to when I watched that episode and add those entry to my google calender event! (maybe i should use kodis default request parameter)
 
 ##### Currently my program can do:
 * Retrive what is playing on my kodi and display them as playing {Serise Name} {Serise season} {Episode}
+* User authanication using google api
+* Push retrived data to google calender using the api. With system time.
+
+#### Things i did not include to the repo:
+* Api credentials.json and token.pickle :)  
---
+++ calenderapi/apipushdata.py
@@ -0,0 +1,107 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import requests
+import json
+from datetime import datetime, timedelta
+# from datetime import date
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+# reusing the code I wrote to retrive what is now playing on my kodi device, specificly episode data.
+
+def what_in_kodi():
+    url = "http://192.168.0.2:8080/jsonrpc"
+
+    # Example echo method
+    payload = {"jsonrpc": "2.0", "method": "Player.GetItem", "params": { "properties": ["title", "album", "artist", "season", "episode", "duration", "showtitle", "tvshowid", "thumbnail", "file", "fanart", "streamdetails"], "playerid": 1 }, "id": "VideoGetItem"}
+    response = requests.post(url, json=payload).json()
+    # print(json.dumps(response['result'], indent=4))
+    with open('response.json', 'w') as outfile:
+        json.dump(response['result'], outfile)
+    with open('response.json') as playing_now:
+        read_content = json.load(playing_now)
+        showname_detail = read_content['item']
+        showname = showname_detail['showtitle']
+        season = showname_detail['season']
+        episode = showname_detail['episode']
+        # return showname, season, episode
+        # here i am retuning the now playig data as a scring with custom formating
+        return("You Watched {} Season {} Episode {}".format(showname, season, episode))
+
+# current time function, this return the date time as a iso formate, this is neccessary to push the valid formate to google api
+def current_Time():
+    my_date = datetime.now()
+    return my_date.isoformat()
+# similar code by this time i am adding 30 minute to my current time. as it's the lenght of the episode i am now playing
+def appro_Watchtime():
+    approwatchtime = datetime.now() + timedelta(hours=0.5)
+    return approwatchtime.isoformat()
+
+# test print approximate watchtime
+# print(appro_Watchtime())
+# Google api code
+
+def googlecalapi():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    # now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    # replacing some dictonary data wtih my function returned data
+    ev = {
+        'summary': 'You Watched',
+        # 'location': '',
+        # 'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '',
+            'timeZone': 'Asia/Dhaka',
+        },
+        'end': {
+            'dateTime': '',
+            'timeZone': 'Asia/Dhaka',
+        },
+    }
+
+    # custom dictonary value
+
+    ev['summary'] = what_in_kodi()
+    ev['start']['dateTime'] = current_Time()
+    ev['end']['dateTime'] = appro_Watchtime()
+
+    # ev = e + what_in_kodi()
+
+    # Pring the event value
+    # print(ev)
+    # pushing the data to google calender my primary calender
+    service.events().insert(calendarId='primary', body=ev).execute()
+
+if __name__ == '__main__':
+    googlecalapi()
+    what_in_kodi()
+    # custom_Function()
